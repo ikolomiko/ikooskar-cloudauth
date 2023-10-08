f7080a8bdf1007548c402654d98fe65d7d274a16
İlker Avcı <ikolomiko@gmail.com>
2023-10-08 18:09:07 +0300

    create models

---
+++ model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from peewee import SqliteDatabase, TextField, Model, IntegerField, DateTimeField
+
+db = SqliteDatabase("database.db")
+
+
+class LicensedUser(Model):
+    serial = TextField(primary_key=True, unique=True, null=False)
+    mac = TextField(null=True, default=None)
+    ip = TextField(null=True, default=None)
+    created_date = DateTimeField(default=datetime.now)
+
+    class Meta:
+        database = db
+
+
+class DemoUser(Model):
+    mac = TextField(primary_key=True, unique=True, null=False)
+    remainings = IntegerField(null=False, default=3)
+    created_date = DateTimeField(default=datetime.now)
+
+    class Meta:
+        database = db
+
+
+def init_db():
+    LicensedUser.create_table()
+    DemoUser.create_table()
